aba904d4fab3cf6fe3b441216dfa0bdfbc518b40
Blanka <38593964+ElMagnificoPro@users.noreply.github.com>
2025-06-11 12:35:06 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,1 @@
 
-if(process.env.production){
-	module.exports = {
-		'url': 'mongodb://lamaisonducrochet:lamaisonducrochet17062009@ds053874.mlab.com:53874/lamaisonducrochet'
-	};
-}else{
-	module.exports = {
-		'url' : 'mongodb://localhost/db'  
-  // 'url': 'mongodb://lamaisonducrochet:lamaisonducrochet17062009@ds053874.mongolab.com:53874/lamaisonducrochet'
-};	
-}
